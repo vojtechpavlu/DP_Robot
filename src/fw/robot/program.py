24ab7792544935fb8c1b74281aa59c5a0bb5a82a
vojtechpavlu <vojtech.pavlu.dev>
2022-03-21 14:41:32 +0100

    Premature program termination definition

--- src/fw/robot/program.py
+++ src/fw/robot/program.py
@@ -4,10 +4,30 @@ manipulaci a běh programu, který řídí robota."""
 # Import standardních knihoven
 from abc import ABC, abstractmethod
 from typing import Iterable
+from enum import Enum
 
 # Import lokálních knihoven
 import src.fw.robot.robot as robot_module
 import src.fw.robot.unit as unit_module
+
+from src.fw.utils.error import PlatformError
+
+
+class AbortType(Enum):
+    """Výčtový typ 'AbortType' definuje podrobnější specifikaci, s jakou
+    byl program ukončen.
+
+    Uvažujeme zde 3 typy ukončení programu z vlastní vůle:
+        - SUCCESS:  Program splnil svůj cíl, splnění rozpoznal a tímto
+                    ukončuje svoji činnost.
+
+        - FAILURE:  Program rozpoznal, že cíle se dosáhnout za daných
+                    okolností nedá; přesto je jeho chování validní.
+
+        - ERROR:    Program se ukončuje, neboť narazil na chybu a upozorňuje
+                    na ní tímto prohlášením.
+    """
+    SUCCESS, FAILURE, ERROR = range(3)
 
 
 class AbstractProgram(ABC):
@@ -53,11 +73,42 @@ class AbstractProgram(ABC):
         """
 
     @abstractmethod
-    def terminate(self):
+    def terminate(self, message: str = "",
+                  abort_type: "AbortType" = AbortType.ERROR):
         """Metoda slouží k předčasnému ukončení robota. Přesněji tím
         program vyjadřuje své ukončení, neboť rozpoznal situaci, kdy je
         vhodné se ukončit. Především je tento způsob vhodný tehdy, když
         robot úspěšně splní svůj cíl, když zjistí, že cíl je nedosažitelný
         nebo když narazí na neřešitelnou chybu."""
+        raise ProgramTermination(message, self, abort_type)
 
+
+class ProgramTermination(PlatformError):
+    """Výjimka reprezentující ukončení z vůle programu. Kromě zprávy je tato
+    výjimka nosičem reference na program, který se ukončil, a na typ ukončení.
+    """
+
+    def __init__(self, message: str, program: AbstractProgram,
+                 abort_type: AbortType):
+        """Initor třídy výjimky, který má za cíl v první řadě zavolat
+        initor předka a uložit potřebné atributy.
+
+        Atributy této výjimky jsou především reference na program, který
+        se rozhodl předčasně ukončit a typ, s jakým se rozhodl program
+        ukončit.
+        """
+        PlatformError.__init__(self, message)
+
+        self._program = program
+        self._abort_type = abort_type
+
+    @property
+    def program(self) -> AbstractProgram:
+        """Vlastnost vrací referenci na program, který se ukončil."""
+        return self._program
+
+    @property
+    def abort_type(self) -> AbortType:
+        """Vlastnost vrací typ, s jakým se program ukončil."""
+        return self._abort_type
 
