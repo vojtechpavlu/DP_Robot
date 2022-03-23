22433eb5a39587f7ec119173f3f36f587fa3c43e
vojtechpavlu <vojtech.pavlu.dev>
2022-03-23 20:16:29 +0100

    PluginValidator as Described

--- src/fw/utils/loading/plugin_validator.py
+++ src/fw/utils/loading/plugin_validator.py
@@ -13,16 +13,31 @@ mají příslušný požadovaný protokol a lze s nimi bezpečně pracovat.
 
 # Import standardních knihoven
 from abc import ABC, abstractmethod
+from typing import Type
 
 # Import lokálních knihoven
-from typing import Type
 import src.fw.utils.loading.plugin as pl
+
+from src.fw.utils.described import Described
 from src.fw.utils.named import Named
 
 
-class PluginValidator(ABC, Named):
+class PluginValidator(ABC, Named, Described):
     """Validátor pluginů, který ověřuje, že dodané pluginy jsou skutečně
     dle dodaných pravidel validní a použitelné v daném kontextu."""
+
+    def __init__(self, validator_name: str, validator_desc: str):
+        """Initor třídy, který je odpovědný za přijetí názvu a popisu
+        validátoru potenciálních pluginů.
+
+        Zatímco název plní funkci spíše lidsky čitelného identifikátoru, popis
+        plní roli předání podrobnějších informaci o validátoru. V popisu by
+        měl být uveden alespoň stručně způsob vyhodnocování, zda je plugin
+        validní či nikoliv, případně pro danou instanci konkrétní specifické
+        hodnoty.
+        """
+        Named.__init__(self, validator_name)
+        Described.__init__(self, validator_desc)
 
     @abstractmethod
     def is_valid_plugin(self, plugin: "pl.Plugin") -> bool:
@@ -36,8 +51,12 @@ class SyntaxValidator(PluginValidator):
     je vyhozena výjimka. Na základě toho lze rozlišit nevalidní moduly."""
 
     def __init__(self):
-        Named.__init__(self, "Syntax Validator")
-        PluginValidator.__init__(self)
+        PluginValidator.__init__(
+            self, "Syntax Validator",
+            f"Syntax Validator se pokusí plugin načíst. Dojde-li během "
+            f"načítání k chybě (typicky syntaktické), bude daný plugin "
+            f"vyhodnocen jako nevalidní."
+        )
 
     def is_valid_plugin(self, plugin: "pl.Plugin") -> bool:
         """Implementace metody je odpovědná za provedení kontroly syntaxe.
@@ -63,8 +82,13 @@ class FunctionExistenceValidator(PluginValidator):
         úspěšně neprojde. Stejně tak v případě, kdy sice modul obsahuje
         atribut tohoto názvu, nikoliv však volatenou funkci.
         """
-        Named.__init__(self, "Function Existence Validator")
-        PluginValidator.__init__(self)
+        PluginValidator.__init__(
+            self, "Function Existence Validator",
+            f"Validátor ověřuje, že modul reprezentující daný plugin má v "
+            f"sobě obsaženu funkci názvu '{function_name}'. Důvodem je snaha "
+            f"o zajištění přístupnosti k některým údajům."
+        )
+
         self._function_name = function_name
 
     @property
@@ -88,8 +112,11 @@ class ModuleDocstringExistenceValidator(PluginValidator):
     def __init__(self):
         """Jednoduchý initor odpovědný za volání initorů svých předků.
         """
-        Named.__init__(self, "Module Docstring Existence Validator")
-        PluginValidator.__init__(self)
+        PluginValidator.__init__(
+            self, "Module Docstring Existence Validator",
+            f"Validátor ověřuje, že modul reprezentovaný dodaným pluginem "
+            f"je opatřen neprázdným dokumentačním komentářem."
+        )
 
     def is_valid_plugin(self, plugin: "pl.Plugin") -> bool:
         """Funkce ověřuje, zda-li dodaný plugin odkazuje na modul s
@@ -114,8 +141,14 @@ class FunctionReturnValueTypeValidator(PluginValidator):
             - ntici parametrů, které mají být do funkce vloženy pro
               očekávaný efekt
         """
-        Named.__init__(self, "Function Return Value Type Validator")
-        PluginValidator.__init__(self)
+        PluginValidator.__init__(
+            self, "Function Return Value Type Validator",
+            f"Validátor ověřuje, že zavolání funkce s názvem "
+            f"'{function_name}' s dodanými parametry '{params}' vrátí hodnotu "
+            f"typu '{to_be_class}'. Pokud ne, bude prohlášen plugin za "
+            f"nevalidní, stejně tak nebude-li mít funkci daného názvu."
+        )
+
         self._function_name: str = function_name
         self._to_be_class: Type = to_be_class
         self._params = params
@@ -160,6 +193,9 @@ class FunctionReturnValueTypeValidator(PluginValidator):
             if plugin.has_function(self.function_name):
                 func = plugin.get_function(self.function_name)
                 return isinstance(func(*self.params), self.to_be_class)
+            else:
+                # Funkce daného názvu není přítomna
+                return False
         except pl.PluginError as ple:
             print(f"CHYBA PŘI VALIDACI NÁVRATOVÉ HODNOTY {ple}")
             return False
