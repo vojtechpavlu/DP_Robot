d0d61f929a60c880a5f69da4f8a042dc14f00b6d
vojtechpavlu <vojtech.pavlu.dev>
2022-03-23 22:03:32 +0100

    Docstring fulfillment and TODO added

--- src/fw/utils/loading/unit_factory_loader.py
+++ src/fw/utils/loading/unit_factory_loader.py
@@ -2,8 +2,6 @@
 
 
 # Import standardních knihoven
-from abc import ABC, abstractmethod
-from typing import Type
 
 # Import lokálních knihoven
 import src.fw.utils.loading.plugin_loader as loader_module
@@ -19,17 +17,24 @@ _ACCESS_FUN = "get_unit_factory"
 
 
 class UnitFactoryLoader(loader_module.PluginLoader):
-    """"""
+    """Loader továren jednotek UnitFactoryLoader je odpovědný za dynamické
+    načítání pluginů obsahujících definici továrních tříd jednotek.
+
+    Třída poskytuje dynamickou správu načítání pluginů v kontextu získávání
+    továrních jednotek. Oproti abstraktnímu předkovi ('PluginLoader') je tato
+    obohacena ještě o implementaci funkce 'load()', která vyfiltruje všechny
+    nevalidní pluginy a vrátí množinu pluginů třídy UnitFactoryPlugin."""
 
     def __init__(self, dest_dir: str,
                  identifiers: "tuple[pl_identifier.PluginIdentifier]",
                  validators: "tuple[pl_validator.PluginValidator]"):
-        """"""
+        """Initor třídy odpovědný za inicializaci předka a uložení
+        všech identifikátorů a validátorů pluginů."""
 
-        """Volání initoru předka"""
+        # Volání initoru předka
         loader_module.PluginLoader.__init__(self, dest_dir)
 
-        """Přidání všech dodaných identifikátorů a validátorů"""
+        # Přidání všech dodaných identifikátorů a validátorů
         self.add_all_identifiers(identifiers)
         self.add_all_validators(validators)
 
@@ -45,26 +50,45 @@ class UnitFactoryLoader(loader_module.PluginLoader):
         validačními procedurami definovanými instancemi třídy PluginValidator.
         """
         valid_plugins = []
+
+        # Pro všechny identifikované (a tedy potenciální) pluginy
         for potential_plg_path in self.potential_plugins:
             plugin = UnitFactoryPlugin(potential_plg_path, self, _ACCESS_FUN)
             if plugin.is_valid_plugin:
                 valid_plugins.append(plugin)
+            # TODO - log nevalidního pluginu
         return tuple(valid_plugins)
 
 
 class UnitFactoryPlugin(plugin_module.Plugin):
-    """"""
+    """Třída UnitFactoryPlugin je odpovědná za zpřístupnění dynamického
+    načítání modulů v kontextu továren jednotek.
+
+    Instance této třídy rozšiřují funkcionalitu rodičovské třídy (tedy Plugin)
+    a mají nad rámec i vlastnost pro získání dané tovární třídy.
+
+    V rámci kontextového způsobu použití lze mluvit o třídě podle návrhového
+    vzoru Služebník."""
 
     def __init__(self, abs_path: str,
                  plugin_loader: "UnitFactoryLoader",
                  access_point_fun: str):
-        """"""
+        """Initor třídy odpovědný za iniciaci svého předka a za uložení
+        všech potřebných hodnot.
+
+        V parametrech přijímá absolutní cestu k modulu, který má být ověřen a
+        z něhož má být čteno, dále referenci na PluginLoader, který tuto
+        instanci tvoří, a také název přístupové funkce, která vrací instanci
+        tovární třídy jednotky.
+        """
         plugin_module.Plugin.__init__(self, abs_path, plugin_loader)
+
         self._access_point_function = access_point_fun
 
     @property
     def access_point_function(self) -> str:
-        """"""
+        """Vlastnost vrací název funkce, kterou je třeba zavolat, aby byla
+        navrácena instance tovární třídy jednotek."""
         return self._access_point_function
 
     @property
@@ -73,15 +97,12 @@ class UnitFactoryPlugin(plugin_module.Plugin):
         daného typu.
         """
 
-        """Přístupová funkce musí být obsažena v daném modulu. Není-li
-        tomu tak, je vyhozena příslušná výjimka."""
+        # Přístupová funkce musí být obsažena v daném modulu. Není-li tomu
+        # tak, je vyhozena příslušná výjimka.
         if not self.has_function(self.access_point_function):
             raise plugin_module.PluginError(
                 f"Plugin nemá přístupovou funkci názvu "
                 f"'{self.access_point_function}'", self)
 
-        """Vrácení výstupu volání přístupové funkce."""
+        # Vrácení výstupu volání přístupové funkce
         return self.get_function(self._access_point_function)()
-
-
-
