217b0e6040e2ff4ea3d24a21dedb2bf1d148d488
vojtechpavlu <vojtech.pavlu.dev>
2022-03-23 19:52:02 +0100

    Task as Described

--- src/fw/target/task.py
+++ src/fw/target/task.py
@@ -8,21 +8,35 @@ tedy jaký má mít protokol.
 
 
 # Import lokálních knihoven
+from src.fw.utils.described import Described
 from src.fw.utils.identifiable import Identifiable
 from src.fw.utils.named import Named
 
 import src.fw.target.evaluation_function as ef_module
 
 
-class Task(Identifiable, Named):
+class Task(Identifiable, Named, Described):
     """Instance třídy Task umožňují sledovat postup při řešení problému daného
     úlohou."""
 
-    def __init__(self, name: str, eval_fun: "ef_module.EvaluationFunction"):
-        """"""
-        Identifiable.__init__(self)
-        Named.__init__(self, name)
+    def __init__(self, task_name: str, task_desc: str,
+                 eval_fun: "ef_module.EvaluationFunction"):
+        """Initor třídy, který přijímá v název úkolu, jeho popis a evaluační
+        funkci, která bude vyhodnocovat jeho splnění.
 
+        Zatímco název plní funkci spíše pro člověka identifikační, podstata
+        dědění třídy Identifiable je spíše za účelem identifikace strojem.
+        Popis plní funkci čistě popisnou. V popisu by měl být uveden (alespoň
+        vágně) kýžený stav, jehož splnění je očekáváno a vyhodnocováno
+        evaluační funkcí.
+        """
+
+        """Volání initorů předků"""
+        Identifiable.__init__(self)
+        Named.__init__(self, task_name)
+        Described.__init__(self, task_desc)
+
+        """Uložení dodané evaluační funkce"""
         self._eval_fun = eval_fun
 
     def eval(self) -> bool:
