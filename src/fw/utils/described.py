15bf9a96005d7ece7f20536877501f7133760a18
vojtechpavlu <vojtech.pavlu.dev>
2022-03-23 19:36:13 +0100

    Docstring fulfillment

--- src/fw/utils/described.py
+++ src/fw/utils/described.py
@@ -1,4 +1,5 @@
-""""""
+"""Modul 'description' definuje obecnou abstraktní třídu coby protokol pro
+všechny instance, které mají vlastní popis v přirozeném jazyce."""
 
 
 # Import standardních knihoven
@@ -6,13 +7,25 @@ from abc import ABC, abstractmethod
 
 
 class Described(ABC):
-    """"""
+    """Abstraktní třída 'Described' slouží k definici společného protokolu
+    všech instancí, které vyžadují popis v člověku čitelném jazyce.
+
+    Instance tříd, které implementují tuto třídu, mají v sobě uložen popis
+    v podobě textového řetězce a jsou schopny tento nabídnout i svému okolí.
+    """
 
     def __init__(self, description: str = ""):
-        """"""
+        """Initor třídy, který přijímá v parametru popis, jenž bude dále
+        reprezentovat člověku čitelnou reprezentaci instance.
+
+        Významem je například zdůvodnit způsob použití, specifikovat
+        konkrétní specifické vlastnosti instance nebo popsat význam.
+        """
         self._desc = description
 
     @property
     def description(self) -> str:
-        """"""
+        """Vlastnost, která vrací textový řetězec reprezentující popis
+        této instance, resp. instance implementující tuto abstraktní třídu.
+        """
         return self._desc
