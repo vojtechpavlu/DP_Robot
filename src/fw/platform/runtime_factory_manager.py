12cc8e5d1ffcd7dd0fb75dd012890c31e2aaef2a
vojtechpavlu <vojtech.pavlu.dev>
2022-03-27 15:56:47 +0200

    RuntimeFactoryManager basic definition

---
+++ src/fw/platform/runtime_factory_manager.py
@@ -0,0 +1,40 @@
+""""""
+
+
+# Import standardních knihoven
+from typing import Iterable
+
+# Import lokálních knihoven
+import src.fw.platform.runtime as runtime_module
+import src.fw.utils.loading.runtime_factory_loader as loader_module
+
+
+class RuntimeFactoryManager:
+    """"""
+
+    def __init__(self, rf_loader: "loader_module.RuntimeFactoryLoader"):
+        """"""
+        self._loader = rf_loader
+        self._registered: "list[runtime_module.AbstractRuntimeFactory]" = []
+
+    @property
+    def loader(self) -> "loader_module.RuntimeFactoryLoader":
+        """"""
+
+    @property
+    def registered_factories(
+            self) -> "tuple[runtime_module.AbstractRuntimeFactory]":
+        """"""
+        return tuple(self._registered)
+
+    @property
+    def num_of_registered(self) -> int:
+        """"""
+        return len(self.registered_factories)
+
+    def register(self, rt_factory: "runtime_module.AbstractRuntimeFactory"):
+        """"""
+
+    def load(self):
+        """"""
+
