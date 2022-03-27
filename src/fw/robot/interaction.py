d0bdff3cb1214ce789ac115dc60007f725955ea4
vojtechpavlu <vojtech.pavlu.dev>
2022-03-28 00:18:42 +0200

    Docstring fulfillment

--- src/fw/robot/interaction.py
+++ src/fw/robot/interaction.py
@@ -1,4 +1,9 @@
-""""""
+"""Modul sdružuje veškerou funkcionalitu ve vztahu k interakcím mezi robotem
+a světem a zpracováním takových interakcí.
+
+Především je zde definice abstraktních tříd samotné interakce, handleru
+interakcí a správce handlerů interakcí. Dále obsahuje definici tříd
+InteractionFactory a InteractionHandlerFactory."""
 
 
 # Import standardních knihoven
@@ -11,11 +16,17 @@ from src.fw.utils.described import Described
 from src.fw.utils.identifiable import Identifiable
 
 import src.fw.robot.unit as unit_module
-import src.fw.world.world_interface as world_interface_module
+import src.fw.world.world_interface as wrld_interf_module
 
 
 class Interaction(ABC, Identifiable, Named, Described):
-    """"""
+    """Abstraktní třída interakce, která definuje nejzákladnější protokol
+    pro všechny interakce, se kterými je možné se v systému setkat.
+
+    Tato třída obsahuje definici způsobu správy jednotky, která je původcem
+    této interakce, stejně jako abstraktní protokol pro funkci, která se
+    stará (podle návrhového vzoru Command) o samotné provedení akce v
+    souvislosti se světem, resp. jeho rozhraním."""
 
     def __init__(self, name: str, desc: str, unit: "unit_module.AbstractUnit"):
         """"""
@@ -26,82 +37,158 @@ class Interaction(ABC, Identifiable, Named, Described):
         self._unit = unit
 
     @property
+    def interaction_type(self) -> "Type":
+        """Vlastnost vrací typ, kterého daná interakce je."""
+        return type(self)
+
+    @property
     def unit(self) -> "unit_module.AbstractUnit":
-        """"""
+        """Vlastnost vrací jednotku, která je původcem této interakce. Pomocí
+        toho je také možné získat referenci na robota, který danou interakci
+        ze svého programu inicioval.
+        """
         return self._unit
 
     @abstractmethod
-    def run(self) -> object:
-        """"""
-        # TODO - nastavení rozhraní světa
+    def execute_interaction(
+            self, interface: "wrld_interf_module.WorldInterface") -> object:
+        """Abstraktní funkce, která se pokusí podle vzoru Command provést
+        danou interakci a vrátit výsledek."""
 
 
 class InteractionHandler(ABC):
-    """"""
+    """Abstraktní třída InteractionHandler definuje obecný protokol pro
+    všechny handlery (zpracovatele) interakcí. To umožňuje dynamické použití
+    interakcí podle návrhového vzoru Command.
+
+    Základním předpokladem je schopnost rozlišení jednotlivých interakcí,
+    za jejichž zpracování je tento handler odpovědný, stejně jako existence
+    autority (kontejneru), který jejich zpracování řídí.
+    """
 
     def __init__(self, interaction_type: "Type"):
-        """"""
+        """Initor třídy, který je odpovědný za uložení dodaného typu interakce,
+        za jejíž zpracování je tento handler odpovědný.
+        """
         self._interaction_type = interaction_type
 
     @property
     def interaction_type(self) -> "Type":
-        """"""
+        """Vlastnost vrací typ interakce, za který je tento handler odpovědný.
+        """
         return self._interaction_type
 
     def is_mine(self, interaction: "Interaction") -> bool:
+        """Funkce vrací, zda-li dodaný handler je odpovědný za zpracování
+        dodané interakce."""
+        return type(interaction) is self.interaction_type
+
+
+class InteractionHandlerFactory(ABC):
+    """Abstraktní třída InteractionHandlerFactory slouží k tvorbě handlerů,
+    které jsou odpovědné za zpracování příslušných interakcí.
+
+    Tato třída stanovuje obecný protokol sestávající se ze dvou základních
+    (abstraktních) funkcí, které je třeba v potomcích implementovat."""
+
+    @property
+    @abstractmethod
+    def interaction_type(self) -> "Type":
+        """Vlastnost vrací typ interakce, za jehož typ je daný handler co do
+        zpracování odpovědný."""
+
+    @abstractmethod
+    def interaction_handler(self) -> "InteractionHandler":
+        """Abstraktní funkce vrací handler, který je odpovědný za zpracování
+        příslušného typu interakce."""
+
+
+class InteractionFactory(ABC):
+    """"""
+
+    @abstractmethod
+    def build_interaction(self) -> "Interaction":
         """"""
-        return type(interaction) is self._interaction_type
 
 
 class InteractionHandlerManager(ABC):
-    """"""
+    """Instance této třídy jsou odpovědné za sdružování a správu handlerů pro
+    zpracování interakcí."""
 
     def __init__(self):
-        """"""
+        """Initor třídy, který je odpovědný především za inicializaci
+        proměnných.
+        """
+
+        # Rozhraní světa, které je definováno až po vytvoření instance
         self._world_interface = None
+
+        """Handlery, které má správce evidovány. V úvodní fázi je evidence 
+        prázdná, tyto jsou dodávány až za běhu životního cyklu instance."""
         self._handlers: "list[InteractionHandler]" = []
 
     @property
     def interaction_handlers(self) -> "tuple[InteractionHandler]":
-        """"""
+        """Vlastnost vrací ntici všech handlerů, které má správce v evidenci.
+        """
         return tuple(self._handlers)
 
     @property
-    def world_interface(self) -> "world_interface_module.WorldInterface":
-        """"""
+    def world_interface(self) -> "wrld_interf_module.WorldInterface":
+        """Vlastnost vrací rozhraní světa, které je tomuto správci přiděleno.
+        """
         return self._world_interface
 
     @world_interface.setter
     def world_interface(
-            self, world_interface: "world_interface_module.WorldInterface"):
-        """"""
-        if self.has_world_interface:
+            self, world_interface: "wrld_interf_module.WorldInterface"):
+        """Vlastnost nastavuje rozhraní světa. Toto rozhraní lze nastavit
+        pouze jednou. Při pokusu o nastavení hodnoty None nebo přenastavení
+        již jednou nastaveného rozhraní je vyhozena výjimka. Tato typicky
+        značí pokus o podvod nebo chybu ve specifikaci budování systému.
+        """
+        # TODO - specifikace výjimek
+        if world_interface is None:
+            raise Exception("Nelze nastavit prázdné rozhraní světa")
+        elif self.has_world_interface:
             raise Exception("Rozhraní světa nelze znovu měnit")
         self._world_interface = world_interface
 
     @property
     def has_world_interface(self) -> bool:
-        """"""
+        """Vlastnost vrací, bylo-li rozhraní světa nastaveno či nikoliv."""
         return self._world_interface is not None
 
     def add_interaction_handler(self, handler: "InteractionHandler"):
-        """"""
+        """Funkce se pokusí přidat dodaný handler do evidence tohoto správce.
+        Pokud již existuje jeden handler pro zpracování interakcí stejného
+        typu je v evidenci obsažen, je vyhozena výjimka.
+        """
         for i_h in self.interaction_handlers:
             if i_h.interaction_type is handler.interaction_type:
                 # TODO - specifikace výjimky
                 raise Exception(
                     f"Nelze mít evidovány dva handlery pro stejný typ "
                     f"interakce: '{handler.interaction_type}'")
+        self._handlers.append(handler)
 
     def has_handler(self, interaction: "Interaction") -> bool:
-        """"""
+        """Funkce se pokusí vyhledat handler odpovědný za zpracování interakcí
+        daného typu. Pokud-že takový není nalezen, je vráceno False, jinak
+        True."""
         for handler in self.interaction_handlers:
             if handler.is_mine(interaction):
                 return True
         return False
 
     def get_handler(self, interaction: "Interaction") -> "InteractionHandler":
-        """"""
+        """Funkce se pokusí vrátit pro danou interakci handler, který je za
+        zpracování této interakce odpovědný.
+
+        Pokud takový není nalezen, je vyhozena výjimka. To typicky značí
+        pokus o podvod nebo chybně definované přidání požadovaných handlerů
+        do evidence.
+        """
         for handler in self.interaction_handlers:
             if handler.is_mine(interaction):
                 return handler
@@ -111,7 +198,7 @@ class InteractionHandlerManager(ABC):
 
     @abstractmethod
     def process_interaction(self, interaction: "Interaction") -> object:
-        """"""
+        """Abstraktní funkce se pokusí o zpracování dané interakce."""
 
 
 
