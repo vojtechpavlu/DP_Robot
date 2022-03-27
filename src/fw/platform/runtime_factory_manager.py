b91037e6735d550e15e578e79cf4b66165823909
vojtechpavlu <vojtech.pavlu.dev>
2022-03-27 16:01:20 +0200

    RuntimeFactoryManager definition fulfillment

--- src/fw/platform/runtime_factory_manager.py
+++ src/fw/platform/runtime_factory_manager.py
@@ -20,6 +20,7 @@ class RuntimeFactoryManager:
     @property
     def loader(self) -> "loader_module.RuntimeFactoryLoader":
         """"""
+        return self._loader
 
     @property
     def registered_factories(
@@ -34,7 +35,19 @@ class RuntimeFactoryManager:
 
     def register(self, rt_factory: "runtime_module.AbstractRuntimeFactory"):
         """"""
+        if rt_factory not in self.registered_factories:
+            self._registered.append(rt_factory)
+        # TODO - LOG již evidované továrny běhových prostředí
 
     def load(self):
         """"""
+        self._registered: "list[runtime_module.AbstractRuntimeFactory]" = []
+
+        for runtime_factory in self.loader.runtime_factories:
+            self.register(runtime_factory)
+
+        if self.num_of_registered == 0:
+            # TODO - specifikace výjimky
+            raise Exception(
+                "Nebyla načtena jediná továrna běhových prostředí.")
 
