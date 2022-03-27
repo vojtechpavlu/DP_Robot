09180fec72ef819b43bd513fbdf7108bb439e3f8
vojtechpavlu <vojtech.pavlu.dev>
2022-03-27 21:21:58 +0200

    Docstring fulfillment

--- src/fw/robot/robot_name_generator.py
+++ src/fw/robot/robot_name_generator.py
@@ -80,30 +80,44 @@ class FamousExplorers(RobotNameGenerator):
 
 
 class RandomRobotName(RobotNameGenerator):
-    """"""
+    """Instance této třídy jsou odpovědné za generování náhodných řetězců
+    o stanovené délce a ze stanovené abecedy."""
 
-    _DEF_ALPHABET = "abcdefghijklmnopqrstuvwxyz123456789"
+    """Výchozí abeceda použitá pro generování názvů robotů."""
+    _DEF_ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789"
 
     def __init__(self, name_length: int = 8, alphabet: str = _DEF_ALPHABET):
-        """"""
+        """Initor, který přijímá délku vytvářených řetězců (názvů) a abecedu,
+        která má být k tomuto účelu použita. Oba parametry jsou nastaveny na
+        defaultní hodnoty a není tedy nutné je poskytovat.
+        """
         self._length = name_length
         self._alphabet = alphabet.split("")
 
         if len(self._alphabet) <= 1:
-            raise Exception("Použitá abeceda musí být delší než 1 znak")
+            raise Exception(
+                f"Použitá abeceda musí být delší než 1 znak: {self.alphabet}")
+        elif name_length <= 0:
+            raise Exception(
+                f"Minimální délka názvu robota je 1 znak: {name_length}")
 
     @property
     def alphabet(self) -> "tuple[str]":
-        """"""
+        """Vlastnost vrací ntici znaků, které jsou použity pro generování
+        názvů robotů."""
         return tuple(self._alphabet)
 
     @property
     def name_length(self) -> int:
-        """"""
+        """Vlastnost vrací délku, kterou mají mít generované řetězce (názvy
+        robotů) co do počtu znaků."""
         return self._length
 
     def get(self) -> str:
-        """"""
+        """Funkce odpovědná za vygenerování příslušného řetězce.
+
+        Generování probíhá pomocí rekurzivního volání, dokud nemá řetězec
+        požadovanou délku. Pokud této dosáhne, je navrácen."""
         gen_name = self.get()
         return (gen_name if len(gen_name) == self.name_length
                 else f"{choice(self.alphabet)}{gen_name}")
