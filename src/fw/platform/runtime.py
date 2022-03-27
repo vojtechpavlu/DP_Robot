0ca6548a335e023c9d4d7975775f4edc0ba77121
vojtechpavlu <vojtech.pavlu.dev>
2022-03-27 12:51:32 +0200

    Self preparing runtime

--- src/fw/platform/runtime.py
+++ src/fw/platform/runtime.py
@@ -8,42 +8,45 @@ from abc import ABC, abstractmethod
 from src.fw.utils.identifiable import Identifiable
 
 import src.fw.world.world as world_module
+import src.fw.world.world_factory as world_fact_module
 import src.fw.target.target as target_module
 
 
 class AbstractRuntime(ABC, Identifiable):
     """"""
 
-    def __init__(self):
+    def __init__(self, target_factory: "target_module.TargetFactory",
+                 world_factory: "world_fact_module.WorldFactory"):
         """"""
 
         Identifiable.__init__(self)
+        self._target_factory = target_factory
+        self._world_factory = world_factory
+        self._world = None
+        self._target = None
 
     @property
     def world(self) -> "world_module.World":
         """"""
 
-    @world.setter
-    def world(self, new_world: "world_module.World"):
+    @property
+    def world_factory(self) -> "world_fact_module.WorldFactory":
         """"""
-        if self.world:
-            # TODO - Specifikace konkrétní výjimky
-            raise Exception(
-                f"Nelze nastavovat již nastavený svět", self)
-        # TODO - nastavení proměnné světa
+        return self._world_factory
 
     @property
     def target(self) -> "target_module.Target":
         """"""
 
-    @target.setter
-    def target(self, new_target: "target_module.Target"):
+    @property
+    def target_factory(self) -> "target_module.TargetFactory":
         """"""
-        if self.target:
-            # TODO - Specifikace konkrétní výjimky
-            raise Exception(
-                f"Nelze nastavovat již nastavený target", self)
-        # TODO - nastavení proměnné světa
+        return self._target_factory
+
+    def generate(self):
+        """"""
+        self._world = self.world_factory.build()
+        self._target = self.target_factory.build()
 
     @abstractmethod
     def run(self):
