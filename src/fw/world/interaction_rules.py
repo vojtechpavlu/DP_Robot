c9a1bfad4230e89ba0aeb89ea1a96034fb8966d7
vojtechpavlu <vojtech.pavlu.dev>
2022-03-28 14:17:46 +0200

    Docstring fulfillment

--- src/fw/world/interaction_rules.py
+++ src/fw/world/interaction_rules.py
@@ -10,7 +10,12 @@ import src.fw.robot.interaction as interaction_module
 
 
 class InteractionRule(ABC):
-    """"""
+    """Abstraktní třída InteractionRule definuje protokol pro všechna
+    pravidla, která ověřují formální správnost interakcí; tedy zda-li je je
+    možné aplikovat.
+
+    Typickými pravidly je například omezení aplikovatelného počtu interakcí.
+    """
 
     @abstractmethod
     def check(self, interaction: "interaction_module.Interaction") -> bool:
@@ -19,10 +24,19 @@ class InteractionRule(ABC):
 
 
 class LimitedCounter(InteractionRule):
-    """"""
+    """Třída LimitedCounter definuje takové pravidlo, které omezuje počet
+    všech aplikovaných interakcí. Pokud tento limitní počet je překročen,
+    jsou další interakce zamítnuty."""
 
     def __init__(self, num_of_allowed: int = 100_000):
-        """"""
+        """Initor, který přijímá maximální počet aplikovatelných interakcí.
+
+        Tento počet je defaultně nastaven na 100 000. Pokud je však dodán
+        záporný počet, je vyhozena výjimka. Nula (0) je validní hodnotou;
+        žádná interakce nebude přijata.
+
+        Dále initor deklaruje aktuální stav (který je roven 0).
+        """
         self.__num_of_allowed = num_of_allowed
         self.__current_state = 0
 
@@ -32,27 +46,27 @@ class LimitedCounter(InteractionRule):
 
     @property
     def num_of_allowed(self) -> int:
-        """"""
+        """Vlastnost vrací horní limit povolených interakcí."""
         return self.__num_of_allowed
 
     @property
     def current_state(self) -> int:
-        """"""
+        """Vlastnost vrací aktuální stav, tedy kolik interakcí bylo celkem
+        aplikováno."""
         return self.__current_state
 
     @property
     def have_left(self) -> int:
-        """"""
+        """Vlastnost vrací rozdíl mezi limitem a aktuálním stavem, tedy kolik
+        interakcí je ještě možné aplikovat.
+        """
         return self.num_of_allowed - self.current_state
 
-    def tick(self) -> int:
-        """"""
-        self.__current_state += 1
-        return self.current_state
-
     def check(self, interaction: "interaction_module.Interaction") -> bool:
-        """"""
-        self.tick()
+        """Funkce ověřující, že je počet doposud aplikovaných interakcí menší,
+        než je stanovený limit. Pokud toto pravidlo naplněno není, vrací False.
+        """
+        self.__current_state += 1
         return self.have_left > 0
 
 
