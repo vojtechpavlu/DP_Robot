f241d2e1474635ec59bb5678725b467ac3821b6b
vojtechpavlu <vojtech.pavlu.dev>
2022-03-29 15:27:34 +0200

    WorldFactory-WorldInterfaceFactory integration

--- src/fw/world/world_factory.py
+++ src/fw/world/world_factory.py
@@ -6,12 +6,26 @@ from abc import ABC, abstractmethod
 
 # Import lokálních knihoven
 import src.fw.world.world as world_module
+import src.fw.world.world_interface as world_if_module
 import src.fw.world.field as field_module
 from src.fw.utils.error import PlatformError
 
 
 class WorldFactory(ABC):
     """Abstraktní tovární třída mající za cíl připravit instanci světa."""
+
+    def __init__(self,
+                 world_if_factory: "world_if_module.WorldInterfaceFactory"):
+        """Initor přijímá v initoru továrnu rozhraní světa, která bude použita
+        pro vytvoření instance 'WorldInterface' příslušné tvořenému světu."""
+        self._world_if_fact = world_if_factory
+
+    @property
+    def world_interface_fact(self) -> "world_if_module.WorldInterfaceFactory":
+        """Vlastnost odpovědná za poskytnutí uložené instance továrny rozhraní
+        světa, která je použita pro potřeby tvorby instancí 'WorldInterface'.
+        """
+        return self._world_if_fact
 
     @abstractmethod
     def build(self) -> "world_module.World":
@@ -26,8 +40,24 @@ class OpenSpaceWorldFactory(WorldFactory):
     plocha složená z navštivitelných políček (cest). Ta je obehnána stěnami
     ze všech 4 stran."""
 
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int,
+                 world_if_fact: "world_if_module.WorldInterfaceFactory"):
+        """Initor třídy, který přijímá šířku a výšku tvořeného světa a továrnu
+        rozhraní světa.
 
+        Šířka a výška v sobě již má započítány okolní stěny, tedy počet cest
+        vypočítáme jako ((width - 2) * (height - 2)). Oba tyto parametry musí
+        mít hodnotu větší nebo rovnu 3, jinak je vyhozena výjimka.
+
+        Továrna rozhraní světa je odpovědná za tvorbu instancí třídy
+        'WorldInterface', které jsou pro takový svět mezivrstvou chránící
+        svět před nekonzistentními a neintegritními stavy při interakci.
+        """
+
+        # Volání initoru předka
+        WorldFactory.__init__(self, world_if_fact)
+
+        # Uložení šířky a výšky světa
         self._width = width
         self._height = height
 
@@ -67,16 +97,26 @@ class OpenSpaceWorldFactory(WorldFactory):
         hraniční (minimální nebo maximální hodnota výšky nebo šířky), je
         na tuto pozici umístěna stěna, jinak cesta.
         """
+        # Seznam všech políček, ze kterých se svět bude sestávat
         all_fields = []
+
+        # Proiterování všemi kombinacemi souřadnic ( O(šířka * výška) )
         for x in range(self.width):
             for y in range(self.height):
+                """Naplnění světa políčky stěn a cest"""
                 if x == 0 or y == 0:
+                    """Levá a dolní hranice - stěna"""
                     all_fields.append(field_module.Wall(x, y))
                 elif (x == self.width - 1) or (y == self.height - 1):
+                    """Pravá a horní hranice - stěna"""
                     all_fields.append(field_module.Wall(x, y))
                 else:
+                    """Cokoliv mezi hranicemi - cesta"""
                     all_fields.append(field_module.Path(x, y))
-        return world_module.World(all_fields)
+
+        """Vrácení vytvořené instance světa se všemi políčky a s továrnou
+        rozhraní světa"""
+        return world_module.World(all_fields, self.world_interface_fact)
 
 
 class WorldFactoryError(PlatformError):
