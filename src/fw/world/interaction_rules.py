c918609ba3abfb789d2b17879ec8fa6d8e810128
vojtechpavlu <vojtech.pavlu.dev>
2022-03-29 12:55:12 +0200

    Docstring fulfillment

--- src/fw/world/interaction_rules.py
+++ src/fw/world/interaction_rules.py
@@ -71,24 +71,41 @@ class LimitedCounter(InteractionRule):
 
 
 class LimitPerInteractionType(InteractionRule):
-    """"""
+    """Instance této třídy definují omezující pravidlo, které stanovuje
+    maximální počet interakcí pro každý typ.
+
+    Toto pravidlo rozšiřuje působnost pravidla definovaného třídou
+    'LimitCounter', které se zaměřuje na celkový počet všech interakcí.
+    """
 
     def __init__(self, num_of_allowed: int = 500):
-        """"""
-        self._num_of_allowed = 500
+        """Initor, který přijímá maximální počet interakcí pro libovolný typ.
+        Pokud je tento limit dovršen, bude daná interakce zamítnuta.
+        """
+        self._num_of_allowed = num_of_allowed
         self._registry = {}
 
     def tick(self, interaction: "interaction_module.Interaction") -> int:
-        """"""
+        """Funkce, která započte interakci do registrace.
+
+        Pokud je tento typ interakce evidován, počet aplikovaných je zvýšen
+        o 1, pokud není, je počet aplikovaných interakcí daného typu nastaven
+        na 1. Nově nastavený počet je dále vrácen.
+        """
         typename = interaction.interaction_type.__name__
         if typename in self._registry:
+            """Pokud je daný typ evidován v registratuře"""
             self._registry[typename] += 1
         else:
+            """Pokud daný typ interakce doposud v registratuře evidován není"""
             self._registry[typename] = 1
         return self._registry[typename]
 
     def check(self, interaction: "interaction_module.Interaction") -> bool:
-        return self.tick(interaction) < self._num_of_allowed
+        """Funkce vrací, zda byl či nebyl překročen limit aplikovatelných
+        interakcí daného typu. Pokud ano, vrací False, pokud ne, vrací True.
+        """
+        return self.tick(interaction) <= self._num_of_allowed
 
 
 
