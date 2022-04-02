7cf3888ec36a38073c05791953a731b63480cbdf
vojtechpavlu <vojtech.pavlu.dev>
2022-04-02 21:30:22 +0200

    Predefinition of template_runtime_factory

---
+++ src/plugins/assignments/template_runtime_factory.py
@@ -0,0 +1,70 @@
+"""Šablonový modul obsahují základní a nejjednodušší definici """
+
+
+# Import potřebných zdrojů
+from src.fw.platform.platform import Platform
+from src.fw.robot.program import AbstractProgram
+from src.fw.robot.robot import RobotFactory, EmptyRobotFactory
+from src.fw.world.world_factory import WorldFactory, OpenSpaceWorldFactory
+from src.fw.world.spawner import SpawnerFactory, CoordinatesSpawnerFactory
+from src.fw.target.target import TargetFactory, AlwaysCompletedTargetFactory
+from src.fw.world.world_interface import (WorldInterfaceFactory,
+                                          DefaultWorldInterfaceFactory)
+from src.fw.platform.runtime import (AbstractRuntime, AbstractRuntimeFactory,
+                                     SingleRobotRuntime)
+
+
+def _get_unit_names() -> "list[str]":
+    """Funkce vrací seznam názvů jednotek, které jsou pro danou úlohu
+    povoleny."""
+    return [""]  # DOPLNIT
+
+
+def _get_robot_factory() -> "RobotFactory":
+    """Funkce vrací novou instanci továrny robotů, která bude použita
+    pro tvorbu robotů v dané úloze."""
+    return EmptyRobotFactory()  # DOPLNIT
+
+
+def _get_spawner_factory() -> "SpawnerFactory":
+    """Funkce vrací novou instanci továrny spawnerů, kterých bude použito
+    pro zasazování robotů do světa."""
+    return CoordinatesSpawnerFactory(1, 1)  # DOPLNIT
+
+
+def _get_target_factory() -> "TargetFactory":
+    """Funkce vrací novou instanci továrny úlohy."""
+    return AlwaysCompletedTargetFactory()
+
+
+def _get_world_interface_factory() -> "WorldInterfaceFactory":
+    """Funkce vrací zcela novou instanci továrny rozhraní světa."""
+    return DefaultWorldInterfaceFactory()
+
+
+def _get_world_factory() -> "WorldFactory":
+    """Funkce vrací novou instanci továrny světa."""
+    return OpenSpaceWorldFactory(10, 10, _get_world_interface_factory(),
+                                 _get_spawner_factory().build())
+
+
+class TemplateRuntimeFactory(AbstractRuntimeFactory):
+    """"""
+
+    def __init__(self):
+        """"""
+        super().__init__(
+            _get_unit_names(),
+            _get_robot_factory(),
+            _get_world_factory(),
+            _get_target_factory())
+
+    def build(self, platform: "Platform",
+              program: "AbstractProgram") -> "AbstractRuntime":
+        return SingleRobotRuntime(
+            self.world_factory, self.target_factory,
+        )
+
+
+
+
