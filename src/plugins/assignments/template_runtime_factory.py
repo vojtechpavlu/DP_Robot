0458ca05fa1b11b33e60ce8bd8f6d79dc0d6747a
vojtechpavlu <vojtech.pavlu.dev>
2022-04-02 21:51:20 +0200

    Template docstring fulfillment

--- src/plugins/assignments/template_runtime_factory.py
+++ src/plugins/assignments/template_runtime_factory.py
@@ -1,4 +1,12 @@
-"""Šablonový modul obsahují základní a nejjednodušší definici """
+"""Šablonový modul obsahují základní a nejjednodušší definici pluginu pro
+tvorbu běhových prostředí.
+
+V tomto modulu je třeba v nejjednodušších implementacích upravit především
+pomocné funkce uvedené v horní části (začínající na '_get_').
+
+Dále je vhodné upravit i dokumentaci. Tento dokumentační komentář je také
+třeba před použitím upravit.
+"""
 
 
 # Import potřebných zdrojů
@@ -17,42 +25,46 @@ from src.fw.platform.runtime import (AbstractRuntime, AbstractRuntimeFactory,
 def _get_unit_names() -> "list[str]":
     """Funkce vrací seznam názvů jednotek, které jsou pro danou úlohu
     povoleny."""
-    return [""]  # DOPLNIT
+    return [""]  # TODO - DOPLNIT
 
 
 def _get_robot_factory() -> "RobotFactory":
     """Funkce vrací novou instanci továrny robotů, která bude použita
     pro tvorbu robotů v dané úloze."""
-    return EmptyRobotFactory()  # DOPLNIT
+    return EmptyRobotFactory()  # TODO - DOPLNIT
 
 
 def _get_spawner_factory() -> "SpawnerFactory":
     """Funkce vrací novou instanci továrny spawnerů, kterých bude použito
     pro zasazování robotů do světa."""
-    return CoordinatesSpawnerFactory(1, 1)  # DOPLNIT
+    return CoordinatesSpawnerFactory(1, 1)  # TODO - DOPLNIT
 
 
 def _get_target_factory() -> "TargetFactory":
     """Funkce vrací novou instanci továrny úlohy."""
-    return AlwaysCompletedTargetFactory()
+    return AlwaysCompletedTargetFactory()  # TODO - DOPLNIT
 
 
 def _get_world_interface_factory() -> "WorldInterfaceFactory":
     """Funkce vrací zcela novou instanci továrny rozhraní světa."""
-    return DefaultWorldInterfaceFactory()
+    return DefaultWorldInterfaceFactory()  # TODO - DOPLNIT
 
 
-def _get_world_factory() -> "WorldFactory":
+def _get_world_factory() -> "WorldFactory":  # TODO - DOPLNIT
     """Funkce vrací novou instanci továrny světa."""
     return OpenSpaceWorldFactory(10, 10, _get_world_interface_factory(),
                                  _get_spawner_factory().build())
 
 
-class TemplateRuntimeFactory(AbstractRuntimeFactory):
-    """"""
+class TemplateRuntimeFactory(AbstractRuntimeFactory):  # TODO - DOPLNIT
+    """Tato třída slouží k zpřístupnění tvorby co nejjednoduššího běhového
+    prostředí. Chování této třídy je silně ovlivněno definicí přístupových
+    funkcí v horní části tohoto modulu.
+
+    Samotná implementace slouží spíše k šablonovým účelům."""
 
     def __init__(self):
-        """"""
+        """Initor třídy, který má za cíl jen připravit svého předka."""
         super().__init__(
             _get_unit_names(),
             _get_robot_factory(),
@@ -61,12 +73,23 @@ class TemplateRuntimeFactory(AbstractRuntimeFactory):
 
     def build(self, platform: "Platform",
               program: "AbstractProgram") -> "AbstractRuntime":
-        """"""
+        """Funkce 'build', která implementuje signaturu stanovenou předkem.
+
+        Cílem této funkce je tvorba konkrétních běhových prostředí na
+        požádání, resp. na zavolání.
+        """
         return SingleRobotRuntime(
             self.world_factory, self.target_factory,
             self.pick_unit_factories(platform, self.available_units_names),
             program, self.robot_factory, platform)
 
 
+def get_runtime() -> "AbstractRuntimeFactory":  # TODO - DOPLNIT
+    """Hlavní přístupová funkce, která vrací továrnu běhového prostředí.
+    Tato funkce (co do existence, funkce a typu návratové hodnoty) je
+    rozhodujícím faktorem pro validátory pluginů v kontextu dynamické
+    tvorby běhových prostředí.
+    """
+    return TemplateRuntimeFactory()
 
 
