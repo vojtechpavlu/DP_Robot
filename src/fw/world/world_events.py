247d7bc1953bf33b20c3028a3ea15490dfe458bd
vojtechpavlu <vojtech.pavlu.dev>
2022-04-05 12:11:42 +0200

    Docstring fulfillment

---
+++ src/fw/world/world_events.py
@@ -0,0 +1,34 @@
+""""""
+
+
+# Import standardních knihoven
+from dataclasses import dataclass
+
+
+# Import lokálních knihoven
+import src.fw.target.event_handling as event_module
+import src.fw.robot.robot as robot_module
+
+
+@dataclass(frozen=True)
+class FieldChangeEvent(event_module.Event):
+    """Datová třída umožňuje zachytit požadované atributy, které jsou v
+    kontextu změny políčka důležité.
+
+    Tato si uchovává pro snazší manipulaci celočíselné souřadnice 'x' a 'y', a
+    dále referenci na robota, kterému se změnilo políčko.
+
+    Kromě toho uchovává (resp. postupuje předkovi) název události."""
+
+    # Souřadnice nového políčka
+    x: int
+    y: int
+
+    # Reference na robota, který se přesunul
+    robot: "robot_module.Robot"
+
+    # Název události
+    event_name: str = "FieldChange"
+
+
+
