577a7579d22206746b91f5b1d1ae51a2d31c3b10
vojtechpavlu <vojtech.pavlu.dev>
2022-04-08 22:43:00 +0200

    Turning units notify on direction change now

--- src/plugins/units/turning/unit_turn_right.py
+++ src/plugins/units/turning/unit_turn_right.py
@@ -14,6 +14,7 @@ from typing import Type
 # Import lokálních knihoven
 from src.fw.robot.interaction import Interaction, InteractionError
 from src.fw.robot.unit import Actuator, AbstractUnitFactory, Sensor
+from src.fw.world.world_events import DirectionChangeEvent
 from src.fw.world.world_interface import WorldInterface
 
 
@@ -85,6 +86,10 @@ class TurnRightInteraction(Interaction):
 
         # Samotné otočení doprava
         rs.direction = rs.direction.turn_right()
+
+        # Vytvoření události
+        interface.notify_all_event_handlers(
+            DirectionChangeEvent(self.robot, rs.direction))
 
 
 class RightTurnerFactory(AbstractUnitFactory):
