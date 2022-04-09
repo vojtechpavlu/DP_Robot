f8d1aa70d8e16e4ca60e63bd32c55c695f0dc751
vojtechpavlu <vojtech.pavlu.dev>
2022-04-09 17:53:29 +0200

    Updated template runtime factory

--- src/plugins/assignments/template_runtime_factory.py
+++ src/plugins/assignments/template_runtime_factory.py
@@ -13,6 +13,7 @@ třeba před použitím upravit.
 from src.fw.platform.platform import Platform
 from src.fw.robot.program import AbstractProgram
 from src.fw.robot.robot import RobotFactory, EmptyRobotFactory
+from src.fw.utils.logging.logger import Logger
 from src.fw.world.world_factory import WorldFactory, OpenSpaceWorldFactory
 from src.fw.world.spawner import SpawnerFactory, CoordinatesSpawnerFactory
 from src.fw.target.target import TargetFactory, AlwaysCompletedTargetFactory
@@ -25,7 +26,7 @@ from src.fw.platform.runtime import (AbstractRuntime, AbstractRuntimeFactory,
 def _get_unit_names() -> "list[str]":
     """Funkce vrací seznam názvů jednotek, které jsou pro danou úlohu
     povoleny."""
-    return [""]  # TODO - DOPLNIT
+    return ["*"]  # TODO - DOPLNIT
 
 
 def _get_robot_factory() -> "RobotFactory":
@@ -53,7 +54,7 @@ def _get_world_interface_factory() -> "WorldInterfaceFactory":
 def _get_world_factory() -> "WorldFactory":  # TODO - DOPLNIT
     """Funkce vrací novou instanci továrny světa."""
     return OpenSpaceWorldFactory(10, 10, _get_world_interface_factory(),
-                                 _get_spawner_factory().build())
+                                 _get_spawner_factory())
 
 
 class TemplateRuntimeFactory(AbstractRuntimeFactory):  # TODO - DOPLNIT
@@ -71,8 +72,8 @@ class TemplateRuntimeFactory(AbstractRuntimeFactory):  # TODO - DOPLNIT
             _get_world_factory(),
             _get_target_factory())
 
-    def build(self, platform: "Platform",
-              program: "AbstractProgram") -> "AbstractRuntime":
+    def build(self, platform: "Platform", program: "AbstractProgram",
+              logger: "Logger") -> "AbstractRuntime":
         """Funkce 'build', která implementuje signaturu stanovenou předkem.
 
         Cílem této funkce je tvorba konkrétních běhových prostředí na
@@ -81,7 +82,7 @@ class TemplateRuntimeFactory(AbstractRuntimeFactory):  # TODO - DOPLNIT
         return SingleRobotRuntime(
             self.world_factory, self.target_factory,
             self.pick_unit_factories(platform, self.available_units_names),
-            program, self.robot_factory, platform)
+            program, self.robot_factory, platform, logger)
 
 
 def get_runtime_factory() -> "AbstractRuntimeFactory":  # TODO - DOPLNIT
