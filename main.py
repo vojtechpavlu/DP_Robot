acb70c8ce93a6870c1cc08989abac9b984796e00
vojtechpavlu <vojtech.pavlu.dev>
2022-04-28 03:57:06 +0200

    Platform holds it's assignment name now

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import time
 
 def _build_platform(assignment: str):
     platform = Platform(
+        assignment,
         [DefaultUnitFactoryLoader()],
         [DefaultProgramLoader(assignment)],
         DefaultRuntimeFactoryLoader(assignment),
