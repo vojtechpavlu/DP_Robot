2e0382e60736f34ac7aa3e10dde6945a711a22da
vojtechpavlu <vojtech.pavlu.dev>
2022-04-29 18:51:15 +0200

    _00_01_D assignment definition

---
+++ src/plugins/assignments/_00_01_D/definition/__init__.py
@@ -0,0 +1,7 @@
+"""Testován je výpis specifického řetězce do výstupu, konkrétně jména robota.
+Pokud nebude použito funkce 'log', bude úloha vyhodnocena jako nesprávně
+řešena. Pokud bude vypsán do funkce 'log' jiný řetězec, než ten požadovaný,
+bude vyhodnocena s úspěšností 50 %."""
+
+
+
