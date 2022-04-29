a92c63916d674deb81e3413c02f0fc7366b1be4a
vojtechpavlu <vojtech.pavlu.dev>
2022-04-29 18:52:21 +0200

    _00_02_D assignment definition

---
+++ src/plugins/assignments/_00_02_D/definition/__init__.py
@@ -0,0 +1,15 @@
+"""V této úloze je požadováno, aby program byl schopen zjistit jméno robota.
+Do výstupní funkce 'log' je tedy třeba dodat jméno zjištěné z dodané instance
+robota, pro kterého program běží.
+
+Toto jméno lze zjistit následujícím příkazem:
+
+    >>> robot.name
+
+Pokud autor použije ve svém programu pouze přímo textového řetězce, který
+odpovídá názvu robota, splní úkol jen částečně. Ověření totiž probíhá ve dvou
+prostředích; v jednom případě je robot pojmenován 'Karel', v druhém 'ELIZA'.
+"""
+
+
+
