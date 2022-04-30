a354b852f2f4713f27a9eeaa35b966011109e3a6
vojtechpavlu <vojtech.pavlu.dev>
2022-04-30 23:00:53 +0200

    _O3_01_C assignment definition

---
+++ src/plugins/assignments/_03_01_C/definition/__init__.py
@@ -0,0 +1,11 @@
+"""Smyslem této úlohy je demonstrovat způsob použití proměnných v programu.
+
+Robot v tomto úkolu je osazen jednotkami 'ForwardMover', 'IsWallSensor' a
+'TurnLeft' a je zasazen do obdélníkového světa.
+
+Cílem je zjistit počet políček, kolik svět má. Tedy kolik políček robot
+cestou napříč světem napočítá.
+"""
+
+
+
