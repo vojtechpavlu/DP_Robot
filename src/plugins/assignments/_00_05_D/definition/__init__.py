79ecf51105039a647e07e80d7242d3da3a80e17e
vojtechpavlu <vojtech.pavlu.dev>
2022-04-30 03:25:18 +0200

    _00_05_D assignment definition

---
+++ src/plugins/assignments/_00_05_D/definition/__init__.py
@@ -0,0 +1,11 @@
+"""V této úloze je požadováno, aby program použil aktuátor.
+
+Cílem je, aby robot provedl změnu ve světě pomocí svého aktuátoru ForwardMover.
+Ten robota posune o políčko kupředu. Stejně jako všechny ostatní aktuátory,
+spouští se pomocí funkce 'execute'.
+
+    >>> robot.get_unit('název-aktuátoru').execute()
+"""
+
+
+
