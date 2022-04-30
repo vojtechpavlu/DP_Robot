f1f9d8f3758db9d83451ced51cd3d4504d23b296
vojtechpavlu <vojtech.pavlu.dev>
2022-04-30 03:22:43 +0200

    _00_04_D assignment definition

---
+++ src/plugins/assignments/_00_04_D/definition/__init__.py
@@ -0,0 +1,11 @@
+"""V této úloze je požadováno, aby program byl schopen dodat robotovi jistou
+míru sebeuvědomění.
+
+Robot je osazen jedinou jednotkou (ForwardMover) a cílem je zaznamenat její
+popis. Jednotka si udržuje svůj popis a dokáže ho poskytnout:
+
+    >>> unit.description
+"""
+
+
+
