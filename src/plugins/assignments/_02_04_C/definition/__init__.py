d4cb25c1dead1dd7d8bdd8a4ef16c15938bbc357
vojtechpavlu <vojtech.pavlu.dev>
2022-04-30 23:59:39 +0200

    _02_04_C assignment definition

---
+++ src/plugins/assignments/_02_04_C/definition/__init__.py
@@ -0,0 +1,14 @@
+"""Smyslem této úlohy je ilustrovat, jak lze provádět cykly. Cyklus je ve své
+podstatě konstrukce, která umožňuje opakovat specifikovaný kód.
+
+Ve světě robota je možné pokládat a zvedat značky. Každá značka může obsahovat
+nějaký text. Pro zjištění, zda políčko má na sobě značku, lze použít jednotky
+'HasMark', tato jednotka vrací True nebo False. K tomu je možné také značky
+číst, k tomu je jednotka 'MarkReader', která vrací textový řetězec, který
+značka obsahuje.
+
+Nynějším úkolem je vyhledat ve světě všechny značky a zaznamenat jejich obsah.
+"""
+
+
+
