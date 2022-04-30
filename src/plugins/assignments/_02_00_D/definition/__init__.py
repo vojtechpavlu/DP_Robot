4e787d7013ef0cbe3fad4032438be4d1c4969d82
vojtechpavlu <vojtech.pavlu.dev>
2022-04-30 15:10:28 +0200

    _02_00_D assignment definition

---
+++ src/plugins/assignments/_02_00_D/definition/__init__.py
@@ -0,0 +1,22 @@
+"""Smyslem této úlohy je ilustrovat, jak lze provádět cykly. Cyklus je ve své
+podstatě konstrukce, která umožňuje opakovat specifikovaný kód.
+
+Nyní si ukážeme, jak funguje cyklus while. Tento cyklus opakuje provádění
+kódu specifikovaného ve svém těle, dokud je splněna podmínka.
+
+Na následující ukázce je tento cyklus demonstrován:
+
+    while [podmínka]:
+        ... [kód, který bude opakován, dokud podmínka platí] ...
+
+Nyní je cílem dojít na konec chodby. Kdybychom to řešili jen pomocí podmínky
+IF, buďto by náš program byl nepřehledný a zbytečně dlouhý nebo by byl
+uzpůsoben jen na omezenou délku takové chodby.
+
+Navrhněte proto program, který bude schopen instruovat robota k dosažení
+jejího konce, to znamená dokud nebude stát před protější stěnou; nehledě na
+délku této chodby.
+"""
+
+
+
